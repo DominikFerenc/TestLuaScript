4914ae1c774c79f8b268bc1a19104fc54a66ffe2
Kapitan Rebrow <36919372+DominikFerenc@users.noreply.github.com>
2020-04-29 12:48:23 +0200

    Update

--- main.lua
+++ main.lua
@@ -1,12 +1,57 @@
-local socket = require ("socket")
-udp = socket.udp()
-if udp:setsockname("192.168.1.101", 2947) then
-	print("Conn")
-	udp:settimeout()
-  data = udp:receive()
-  if data then
-    print("Received Data: ", data)
+function getFrame()
+  local socket = require("socket")
+  udp = socket.udp()
+  if udp:setsockname("192.168.1.1", 2947) then
+    print("Connected to HTTP server")
+    
+    while true do
+      data = udp:receivefrom()
+      checkPinOutput(data)
+      if data then
+        print("\nReceived Data: ", data)
+      else
+        print("No recived data")
+      end
+    end
+  else
+    print("Not Connected to HTTP Server")
   end
-else
-	print("Not Conn")
 end
+
+function checkPinOutput(data)
+  button = os.execute("gpio.sh get DOUT1\n")
+  print("gpio button status:", button, "\n")
+  setNewFrame(button, data)
+end
+
+function setNewFrame(button, data)
+  number = "nummber"
+  Error = "Erorrx1"
+
+  if button == 1 then
+    newData = data..",".. number..",".. Error
+    print("new data:", newData)
+    sendNewFrame(newData)
+  else 
+    sendNewFrame(data)
+  end
+end
+
+function sendNewFrame(newData)
+  local socket = require("socket")
+  udp = socket.udp()
+
+  if udp:setsockname("", 80) then
+    print ("Connected to host")
+    udp:send(newData)
+  else 
+    print("Not connected to host")
+  end
+
+  print("New Frame: ", newData)
+end
+
+
+
+
+getFrame()
